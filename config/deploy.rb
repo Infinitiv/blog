178e98c062a324e5359a77ce7391de6118d29de7
Vladimir Markovnin <markovnin@gmail.com>
2014-03-10 22:24:20 +0400

    Линкование producton db

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/var/www/blog'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml db/production.sqlite3}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
